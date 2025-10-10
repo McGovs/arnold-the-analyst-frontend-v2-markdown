2f5d49802fec6a011e03b9107951924692b33ca2
McGovs <76825827+McGovs@users.noreply.github.com>
2025-10-10 15:43:37 +0100

    Update Description.tsx

--- src/pages/Description.tsx
+++ src/pages/Description.tsx
@@ -14,7 +14,7 @@ export default function Description() {
 
         <div className="bg-white p-6 rounded-lg shadow-sm mb-6 space-y-4">
           <p className="text-gray-700">
-            Arnold is an always on analyst that lets your team <span className="font-medium">ask plain-English questions about GA4 and get trustworthy
+            Arnold is an always-on analyst that lets your team <span className="font-medium">ask plain-English questions about GA4 and get trustworthy
             answers within minutes</span>. Built by a digital analytics expert with more than eight years of experience navigating GA4’s quirks and data gaps, <span className="font-medium">Arnold delivers senior-level analysis at software speed</span>.
           </p>
 
