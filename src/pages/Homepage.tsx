da32baf27fc96ea29bc9016818642be1cf22dd00
McGovs <76825827+McGovs@users.noreply.github.com>
2026-01-31 18:54:32 +0000

    Update Homepage.tsx

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -73,7 +73,7 @@ export default function Homepage() {
           <Feature 
             icon={<MessageCircle />} 
             title="Natural Language" 
-            description="Ask qns in plain English. No need to touch the Google Analytics UI or BigQuery."
+            description="Ask in plain English. No need to touch the Google Analytics UI or BigQuery."
           />
           <Feature 
             icon={<Zap />} 
