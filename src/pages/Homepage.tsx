5dac6296e2c152fb0f8e9a5f61892df8d5e6faa8
McGovs <76825827+McGovs@users.noreply.github.com>
2026-01-31 10:27:06 +0000

    Update Homepage.tsx

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -85,7 +85,7 @@ export default function Homepage() {
               Save{" "}
             </span>
             <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-indigo-600">
-              20 hours{" "}
+              15 hours{" "}
             </span>
             <span className="text-slate-900 font-bold">
                every month.
