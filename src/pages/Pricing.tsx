b4f67cb1d7e7130cb99562b1e36c27803e646365
McGovs <76825827+McGovs@users.noreply.github.com>
2026-02-03 11:42:48 +0000

    Update Pricing.tsx

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -147,7 +147,7 @@ export default function Pricing() {
             <>
               Annual Pricing (
               <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-indigo-600">
-              🥇 2 Months Free
+              2 Months Free
               </span>
               )
             </>
